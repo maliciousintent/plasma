e65d302b4e796dfb5d3930d2f494c373e38b8840
Alessio Zappa <alessio@plasticpanda.com>
2013-12-18 09:54:05 +0100

    added handy infinite-scrolling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,48 @@
-/**
- * Main JS file for Casper behaviours
- */
+/*jshint browser:true, laxcomma:true, eqnull:true, indent:2, unused:true, undef:true, devel:true, jquery:true*/
+/*globals jQuery, window */
 
-/*globals jQuery, document */
 (function ($) {
-    "use strict";
 
-    $(document).ready(function(){
+  /**
+   * Main JS file for Plasma behaviours
+   */
 
-        // On the home page, move the blog icon inside the header 
-        // for better relative/absolute positioning.
+  "use strict";
 
-        //$("#blog-logo").prependTo("#site-head-content");
+  $(document).ready(function(){
 
-    });
+    // On the home page, move the blog icon inside the header 
+    // for better relative/absolute positioning.
+
+    //$("#blog-logo").prependTo("#site-head-content");
+
+
+    // easy infinite-scroll ;D
+    $(window).scroll(function () {
+      var _nextPosts = $('.pagination a.older-posts');
+      if (_nextPosts.length > 0) {
+        var _nextPostsLink = $(_nextPosts).attr('href');
+        if ($(window).scrollTop() == ($(document).height() - $(window).height())) {
+          $.ajax({
+            url: _nextPostsLink,
+            success: function (data) {
+              if (data) {
+                var _posts = $(data).find('article')
+                  , _nextPagination = $(data).find('.pagination')
+                  ;
+                $('.pagination').remove();
+                $(_posts).each(function (ip, post) {
+                  $(post).appendTo('.content');
+                });
+                $('.content').append($(_nextPagination));
+                $('.content .pagination').hide();
+              }
+            }
+          });
+        }
+      }
+    });    
+
+  });
 
 }(jQuery));
